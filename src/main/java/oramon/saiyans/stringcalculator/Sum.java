5918371f6f254c189afec3809d0c65f67c5b74eb
Javier Garcia <javier.garcia@oramon.nl>
2017-06-19 16:54:24 +0200

    Operation test to sum to zero passed

--- src/main/java/oramon/saiyans/stringcalculator/Sum.java
+++ src/main/java/oramon/saiyans/stringcalculator/Sum.java
@@ -5,6 +5,17 @@ public class Sum implements Operation {
     private final int first_number;
     private final int second_number;
 
+    public Sum(int first_number, int second_number) {
+        super();
+        this.first_number = first_number;
+        this.second_number = second_number;
+    }
+
+    public Integer calculate() {
+        return first_number + second_number;
+    }
+
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -21,16 +32,6 @@ public class Sum implements Operation {
         int result = first_number;
         result = 31 * result + second_number;
         return result;
-    }
-
-    public Sum(int first_number, int second_number) {
-        super();
-        this.first_number = first_number;
-        this.second_number = second_number;
-    }
-
-    public Integer calculate() {
-        return null;
     }
 
 
