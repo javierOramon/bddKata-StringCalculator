1464386f9a1201b64cfdccae656aaefd3db579f4
Javier Garcia <javier.garcia@oramon.nl>
2017-06-20 16:57:13 +0200

    Passed dynamic delimeters sceneario

--- src/main/java/oramon/saiyans/stringcalculator/OperationFactory.java
+++ src/main/java/oramon/saiyans/stringcalculator/OperationFactory.java
@@ -6,14 +6,46 @@ import java.util.stream.Collectors;
 
 public class OperationFactory {
 
+    public static final String DEFAULT_DELIMETER = ",";
+    public static final String LINE_BREAK = "\n";
+    public static final String DEFINE_DELIMETER = "//";
+
     public Operation create(String input) {
 
         if(input.trim().isEmpty()) return new Sum(0, 0);
-        String sanitizedInput = input.replace("\n", ",");
-        String[] numbers_splitted = sanitizedInput.split(",");
+        String delimeter = extractDelimeter(input);
+        String sanitizedInput = extractOperation(input, delimeter);
+        String[] numbers_splitted = sanitizedInput.split(delimeter);
         List<Integer> numbers = Arrays.stream(numbers_splitted)
                 .map(entry -> Integer.parseInt(entry.trim()))
                 .collect(Collectors.toList());
         return new Sum(numbers);
+    }
+
+    private String extractOperation(String input, String delimeter) {
+        String sanitizedInput = extractStringNumbers(input);
+        sanitizedInput = sanitizedInput.replace(LINE_BREAK, delimeter);
+        return sanitizedInput;
+    }
+
+    private String extractStringNumbers(String input) {
+        String sanitizedInput;
+        if(input.startsWith(DEFINE_DELIMETER)){
+            int position_of_line_break = input.indexOf(LINE_BREAK);
+            sanitizedInput = input.substring(position_of_line_break + 1);
+        }else{
+            sanitizedInput = input.toString();
+        }
+        return sanitizedInput;
+    }
+
+    private String extractDelimeter(String input) {
+        String delimeter = DEFAULT_DELIMETER;
+        if(input.startsWith(DEFINE_DELIMETER)){
+            int first_line_break = input.indexOf(LINE_BREAK);
+            delimeter = input.substring(2, first_line_break);
+        }
+
+        return delimeter;
     }
 }
--- src/test/java/resources/cucumber/add.feature
+++ src/test/java/resources/cucumber/add.feature
@@ -35,4 +35,4 @@ Feature: Add method
         # The delimiter should be followed after "//" and a new line separating the delimiter and the numbers. E.g. //[delimiter]\n[numbers…]
     Given you input of "//;\n1;2"
     When you process this input
-    Then the result will be 3 where the default delimiter is ';'
+    Then the result will be 3
