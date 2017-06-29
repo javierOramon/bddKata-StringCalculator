f6ce37854a8fa2aa268049ce3e5174cfb39c8637
Javier Garcia <javier.garcia@oramon.nl>
2017-06-29 15:42:55 +0200

    Passed test for the equality of Sums

--- src/main/java/oramon/saiyans/stringcalculator/calculate/Sum.java
+++ src/main/java/oramon/saiyans/stringcalculator/calculate/Sum.java
@@ -1,12 +1,32 @@
 package oramon.saiyans.stringcalculator.calculate;
 
-public class Sum implements Operation {
-    public Sum(int i, int i1) {
+import java.util.Arrays;
+import java.util.List;
 
+public class Sum implements Operation {
+    private final List<Integer> operands;
+
+    public Sum(int operand1, int operand2) {
+        operands = Arrays.asList(operand1, operand2);
     }
 
     @Override
     public Double calculate() {
         return null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Sum sum = (Sum) o;
+
+        return operands != null ? operands.equals(sum.operands) : sum.operands == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return operands != null ? operands.hashCode() : 0;
     }
 }
