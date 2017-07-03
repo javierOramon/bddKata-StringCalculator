d68e5e9305c57e41fa57b68552b20784ddc7aca7
Javier Garcia <javier.garcia@oramon.nl>
2017-07-03 17:00:57 +0200

    Added test to don't allow negative values in the operationfactory

--- src/main/java/oramon/saiyans/stringcalculator/calculate/OperationFactory.java
+++ src/main/java/oramon/saiyans/stringcalculator/calculate/OperationFactory.java
@@ -2,46 +2,71 @@ package oramon.saiyans.stringcalculator.calculate;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import oramon.saiyans.NotNegativesAllowed;
 
 import java.util.Map;
-import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class OperationFactory {
 
-    private final ImmutableMap<String, BiFunction<Double, Double, Operation>> map;
+    private final ImmutableMap<String, Function<String, Operation>> map;
+    private final int maxNumber;
 
     public OperationFactory(){
-        map = ImmutableMap.<String, BiFunction<Double, Double, Operation>>builder()
+        map = ImmutableMap.<String, Function<String, Operation>>builder()
                 .put("+", this::buildSum)
-                .put("-", this::buildSubstraction)
                 .put("/", this::buildDivision)
+                .put("-", this::buildSubstraction)
                 .build();
+        maxNumber = 1000;
     }
 
-    private Operation buildDivision(Double operand1, Double operand2) {
+    private Operation buildDivision(String text_input) {
+        String text_prepared = text_input.replace(" ", "").replace("/", " ");
+        String[] operands = text_prepared.split(" ");
+        double operand1 = convertToNumber(operands[0]);
+        double operand2 = convertToNumber(operands[1]);
         return new Division(operand1, operand2);
     }
 
-    private Operation buildSubstraction(double operand1, double operand2) {
+    private Operation buildSubstraction(String text_input) {
+        if(text_input.indexOf("-") != text_input.lastIndexOf("-")) throw new NotNegativesAllowed();
+
+        String text_prepared = text_input.replace(" ", "").replace("-", " ");
+        String[] operands = text_prepared.split(" ");
+        double operand1 = convertToNumber(operands[0]);
+        double operand2 = convertToNumber(operands[1]);
         return new Substraction(operand1, operand2);
     }
 
-    private Operation buildSum(double operand1, double operand2) {
+    private Operation buildSum(String text_input) {
+        String text_prepared = text_input.replace(" ", "").replace("+", " ");
+        String[] operands = text_prepared.split(" ");
+        double operand1 = convertToNumber(operands[0]);
+        double operand2 = convertToNumber(operands[1]);
         return new Sum(operand1, operand2);
     }
 
     public Operation create(String text_input) {
         Operation operation = null;
-        ImmutableSet<Map.Entry<String, BiFunction<Double, Double, Operation>>> set = map.entrySet();
-        for (Map.Entry<String, BiFunction<Double, Double, Operation>> entry : set) {
+        ImmutableSet<Map.Entry<String, Function<String, Operation>>> set = map.entrySet();
+        for (Map.Entry<String, Function<String, Operation>> entry : set) {
             if (text_input.contains(entry.getKey())){
-                String text_prepared = text_input.replace(" ", "").replace(entry.getKey(), " ");
-                String[] operands = text_prepared.split(" ");
-                double operand1 = Double.parseDouble(operands[0].trim());
-                double operand2 = Double.parseDouble(operands[1].trim());
-                operation = entry.getValue().apply(operand1, operand2);
+                operation = entry.getValue().apply(text_input);
             }
         }
         return operation;
+    }
+
+    private Double convertToNumber(String operand) {
+        double number = Double.parseDouble(operand.trim());
+        if(number < 0d){
+            throw new NotNegativesAllowed();
+        }
+
+        if(number > maxNumber){
+            number = 1;
+        }
+        return number;
     }
 }
--- src/test/java/oramon/saiyans/stringcalculator/calculate/OperationFactoryTest.java
+++ src/test/java/oramon/saiyans/stringcalculator/calculate/OperationFactoryTest.java
@@ -1,8 +1,11 @@
 package oramon.saiyans.stringcalculator.calculate;
 
+import oramon.saiyans.NotNegativesAllowed;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class OperationFactoryTest {
 
@@ -28,6 +31,19 @@ public class OperationFactoryTest {
         Operation operation = factory.create(text_input);
         //Assert
         assertEquals("The operation is not the same", expected , operation);
+    }
+
+    @Test
+    public void ASubstractionWithNegativeValuesReturnAnError(){
+        //Arrange
+        OperationFactory factory = new OperationFactory();
+        String text_input = "-4 - 5";
+        //Act
+        try{
+            Operation operation = factory.create(text_input);
+        }catch (NotNegativesAllowed e){
+            assertTrue(true);
+        }
     }
 
     @Test
