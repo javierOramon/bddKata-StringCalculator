2d9d12dc0b221bddfba538248c2a9eafaac590c0
Javier Garcia <javier.garcia@oramon.nl>
2017-07-03 12:01:39 +0200

    Passes all tests for the Substraction operation

--- src/main/java/oramon/saiyans/stringcalculator/calculate/Substraction.java
+++ src/main/java/oramon/saiyans/stringcalculator/calculate/Substraction.java
@@ -1,12 +1,44 @@
 package oramon.saiyans.stringcalculator.calculate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Substraction implements Operation {
 
-    public Substraction(double v, double v1) {
+    private final List<Double> operands;
+
+    public Substraction(double operand1, double operand2) {
+        operands = Arrays.asList(operand1, operand2);
+    }
+
+    public Substraction(List<Double> operands) {
+        this.operands = new ArrayList<>(operands);
     }
 
     @Override
     public Double calculate() {
-        return null;
+        if(operands.isEmpty()) return 0d;
+
+        Double result = operands.get(0);
+        for(int position = 1; position < operands.size(); position++){
+            result -= operands.get(position);
+        }
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Substraction that = (Substraction) o;
+
+        return operands != null ? operands.equals(that.operands) : that.operands == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return operands != null ? operands.hashCode() : 0;
     }
 }
---
+++ src/test/java/oramon/saiyans/stringcalculator/calculate/SubstractionTest.java
@@ -0,0 +1,87 @@
+package oramon.saiyans.stringcalculator.calculate;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class SubstractionTest {
+
+    @Test
+    public void operationEquality(){
+        //Arrange
+        Operation expected = new Substraction(1d, 2d);
+        //Act
+        Operation result = new Substraction(1d, 2d);
+        //Assert
+        assertEquals("Both objects aren't equals", expected, result);
+    }
+
+    @Test
+    public void calculateSubstractionOfOneToZero() throws Exception {
+        //Arrange
+        final Double one = 1d;
+        Operation substraction = new Substraction(1d, 0d);
+        //Act
+        Double result = substraction.calculate();
+        //Assert
+        assertEquals("Substraction to zero doesn't work", one, result);
+    }
+
+    @Test
+    public void calculateSubstractionOfZeroToOneReturnLessOne() throws Exception {
+        //Arrange
+        final Double one = -1d;
+        Operation substraction = new Substraction(0d, 1d);
+        //Act
+        Double result = substraction.calculate();
+        //Assert
+        assertEquals("Substraction to zero doesn't return -1", one, result);
+    }
+
+    @Test
+    public void calculateSubstractionOfTwoNumbers() throws Exception {
+        //Arrange
+        final Double expected = -1d;
+        Operation substraction = new Substraction(1d, 2d);
+        //Act
+        Double result = substraction.calculate();
+        //Assert
+        assertEquals("Substraction with two numbers doesn't work", expected, result);
+    }
+
+    @Test
+    public void calculateSubstractionWithOneNegativeValue() throws Exception {
+        //Arrange
+        final Double expected = -3d;
+        Operation Substraction = new Substraction(-1d, 2d);
+        //Act
+        Double result = Substraction.calculate();
+        //Assert
+        assertEquals("Substraction with one negative value doesn't work", expected, result);
+    }
+
+    @Test
+    public void calculateSubstractionWithNegativeValues() throws Exception {
+        //Arrange
+        final Double expected = 1d;
+        Operation Substraction = new Substraction(-1d, -2d);
+        //Act
+        Double result = Substraction.calculate();
+        //Assert
+        assertEquals("Substraction with two negatives values doesn't work", expected, result);
+    }
+
+    @Test
+    public void calculateMultipleNumbers() throws Exception {
+        //Arrange
+        final Double expected = -7d;
+        Operation Substraction = new Substraction(Arrays.asList(-5d, 2d, 5d, -5d));
+        //Act
+        Double result = Substraction.calculate();
+        //Assert
+        assertEquals("Substraction with multiple values doesn't work", expected, result);
+    }
+
+}
