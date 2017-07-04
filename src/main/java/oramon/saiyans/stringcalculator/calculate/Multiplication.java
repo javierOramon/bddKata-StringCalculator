5c0d95ace9d19ba45fc28feeae4c2d139af220ff
Javier Garcia <javier.garcia@oramon.nl>
2017-07-04 13:11:51 +0200

    Prepared the tests for the multiplications passed

--- src/main/java/oramon/saiyans/stringcalculator/calculate/Multiplication.java
+++ src/main/java/oramon/saiyans/stringcalculator/calculate/Multiplication.java
@@ -1,23 +1,35 @@
 package oramon.saiyans.stringcalculator.calculate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 public class Multiplication implements Operation {
 
+    private final List<Double> operands;
+
     public Multiplication(double operand1, double operand2) {
+        this.operands = Arrays.asList(operand1, operand2);
     }
 
     public Multiplication(Collection<Double> operands) {
-
+        this.operands = new ArrayList<>(operands);
     }
 
     @Override
     public Collection<Double> operands() {
-        return null;
+        return new ArrayList<>(this.operands);
     }
 
     @Override
     public Double calculate() {
-        return null;
+        if(this.operands.isEmpty()) return 0d;
+
+        Double result = operands.get(0);
+        for(int position = 1; position < operands.size(); position++){
+            result *= operands.get(position);
+        }
+        return result;
     }
 }
